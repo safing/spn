9eb33571b4b702d7547c61493f50bb39b338ef68
Daniel <dhaavi@users.noreply.github.com>
2021-12-23 16:49:00 +0100

    Cut off far away Hubs when searching for nearest

--- navigator/findnearest_test.go
+++ navigator/findnearest_test.go
@@ -76,3 +76,39 @@ func findFakeHomeHub(m *Map) {
 	// Recalculate reachability.
 	m.recalculateReachableHubs()
 }
+
+func TestNearbyPinsCleaning(t *testing.T) {
+	testCleaning(t, []int{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}, 5)
+	testCleaning(t, []int{10, 11, 12, 13, 14, 15, 70, 80, 90, 100}, 4)
+	testCleaning(t, []int{10, 11, 12, 13, 14, 15, 16, 80, 90, 100}, 3)
+	testCleaning(t, []int{10, 11, 12, 13, 14, 15, 16, 17, 90, 100}, 3)
+}
+
+func testCleaning(t *testing.T, proximities []int, expectedLeftOver int) {
+	t.Helper()
+
+	nb := &nearbyPins{
+		minPins:     3,
+		maxPins:     5,
+		cutOffLimit: 50,
+	}
+
+	// Simulate usage.
+	for _, prox := range proximities {
+		// Add to list.
+		nb.add(nil, prox)
+
+		// Clean once in a while.
+		if len(nb.pins) > nb.maxPins {
+			nb.clean()
+		}
+	}
+	// Final clean.
+	nb.clean()
+
+	// Check results.
+	t.Logf("result: %+v", nb.pins)
+	if len(nb.pins) != expectedLeftOver {
+		t.Fatalf("unexpected amount of left over pins: %+v", nb.pins)
+	}
+}
