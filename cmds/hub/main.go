758993147643ce5d4959b702f3d1f028993ffc27
Daniel <dhaavi@users.noreply.github.com>
2022-11-04 15:58:33 +0100

    Configure microtask threshold

--- cmds/hub/main.go
+++ cmds/hub/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"runtime"
 
 	"github.com/safing/portbase/info"
 	"github.com/safing/portbase/metrics"
@@ -26,6 +27,28 @@ func main() {
 
 	// Disable module management, as we want to start all modules.
 	modules.DisableModuleManagement()
+
+	// Configure microtask threshold.
+	// Scale with CPU/GOMAXPROCS count, but keep a baseline and minimum:
+	// CPUs -> MicroTasks
+	//    0 ->  8 (increased to minimum)
+	//    1 ->  8 (increased to minimum)
+	//    2 ->  8
+	//    3 -> 10
+	//    4 -> 12
+	//    8 -> 20
+	//   16 -> 36
+	//
+	// Start with number of GOMAXPROCS.
+	microTasksThreshold := runtime.GOMAXPROCS(0) * 2
+	// Use at least 4 microtasks based on GOMAXPROCS.
+	if microTasksThreshold < 4 {
+		microTasksThreshold = 4
+	}
+	// Add a 4 microtask baseline.
+	microTasksThreshold += 4
+	// Set threshold.
+	modules.SetMaxConcurrentMicroTasks(microTasksThreshold)
 
 	// adapt portmaster updates module
 	updates.UserAgent = "Hub"
