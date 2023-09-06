ba1c58c01dc91188f81dae983f52fe6123ba160c
Daniel <dhaavi@users.noreply.github.com>
2023-09-06 13:13:48 +0200

    Bump hub version

--- cmds/hub/main.go
+++ cmds/hub/main.go
@@ -18,7 +18,7 @@ import (
 )
 
 func main() {
-	info.Set("SPN Hub", "0.6.6", "AGPLv3", true)
+	info.Set("SPN Hub", "0.6.18", "AGPLv3", true)
 
 	// Configure metrics.
 	_ = metrics.SetNamespace("hub")
